65f433f7fc272e291c96fb02266c732fc1dc268c
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-01-07 02:38:12 +0300

    basic structure project created

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Introduce.css";
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <div className="introduce-container">
+        <img
+          src="https://media.istockphoto.com/id/1488144839/vector/quiz-logo-quiz-time-label-with-question-mark-quiz-emblem-for-business-marketing-and.jpg?s=612x612&w=0&k=20&c=eYgHRl9M7FtpLRCS-ZKABj3IU5E62cgGUTCY2HN5JO8="
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
