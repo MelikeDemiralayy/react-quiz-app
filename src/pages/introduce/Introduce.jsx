588882e0c813bfedbd1eede8ddbca8043e9ab489
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-01-07 17:28:46 +0300

    added dropdown menü

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Introduce.css";
 import Dropdown from "../../components/dropdown/Dropdown";
+import { useNavigate } from "react-router-dom";
 const Introduce = () => {
   const difficulty = ["easy", "medium", "hard"];
+  const [difficultyChange, setDifficultyChange] = useState("");
+  const navigate = useNavigate();
+  const TOTAL_QUESTİONS = 10;
+
+  const startQuiz = () => {
+    if (difficultyChange) {
+      navigate(`/quiz/${difficultyChange}/${TOTAL_QUESTİONS}`); // Dinamik yönlendirme işlemleri yapmak için
+    }
+  };
   return (
     <div className="introduce">
       <div className="introduce-container">
@@ -10,8 +20,10 @@ const Introduce = () => {
           src="https://t3.ftcdn.net/jpg/03/59/62/74/360_F_359627491_x8biSLaSBHJdxMoKahFd6xlecILJlfRY.jpg"
           alt=""
         />
-        <Dropdown />
-        <div className="introduce-btn">Quiz e Başla</div>
+        <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange} />
+        <div onClick={startQuiz} className="introduce-btn">
+          Quiz e Başla
+        </div>
       </div>
     </div>
   );
