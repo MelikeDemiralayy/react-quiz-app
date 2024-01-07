9d524f9c962acd98ed959b7002ec2bea106b3a6f
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-01-07 21:03:43 +0300

    feat: Added API integration Used the shuffleArray function to randomize answers

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Quiz.css";
+import { useParams } from "react-router-dom";
+import * as api from "../../api/api";
 const Quiz = () => {
-  return <div className="quiz">Quiz</div>;
+  const { difficulty, amount } = useParams();
+  const [questionsData, setQuestionsData] = useState([]); //ekranda dönen dataları tutmak için
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await api.fetchQuizData(difficulty, amount);
+      setQuestionsData(data);
+    };
+    getData();
+  }, []);
+  console.log(questionsData, "questionsData");
+
+  return (
+    <div className="quiz">
+      {difficulty} - {amount}
+    </div>
+  );
 };
 
 export default Quiz;
